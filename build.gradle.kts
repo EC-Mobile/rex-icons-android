8c14a7b54c1110c3c85611e526016d8f9c38a4e2
Zhengqing Zhang <zhengqing.zhang@rakuten.com>
2024-03-19 11:53:48 +0900

    Upgrade AGP to 8.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.maven.publish.base) apply false
     alias(libs.plugins.svgConverter.root)
